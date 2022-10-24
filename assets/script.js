fbd7ae99b57b7281797bb6825805ad522bcb9395
tcstl95 <tomcarlson171@gmail.com>
2022-10-23 21:48:09 -0500

    Commit for BCS help on display data

--- assets/script.js
+++ assets/script.js
@@ -30,27 +30,25 @@ function getWeatherApi(event) {
       var lonVal = weaRes.coord.lon;
 
       var lonlatWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latVal}&lon=${lonVal}&units=imperial&appid=${APIKey}`;
-      fetch(lonlatWeatherURL).then(function (response) {
-        if (!response.ok) {
-          console.log("Weather not found.");
-          resultWeatherEl.innerHTML =
-            "<h3> No weather results found, try again!</h3>";
-          throw response.json();
-        }
+      fetch(lonlatWeatherURL)
+        .then(function (response) {
+          if (!response.ok) {
+            console.log("Weather not found.");
+            resultWeatherEl.innerHTML =
+              "<h3> No weather results found, try again!</h3>";
+            throw response.json();
+          }
           return response.json();
-        
-      })
-      .then(function(weatherData){
-        console.log(weatherData)
-        for(var i=0; i < weatherData.list.length; i+= 8){
+        })
+        .then(function (weatherData) {
+          console.log(weatherData);
+          for (var i = 0; i < weatherData.list.length; i += 8) {
             printWeatherApi(weatherData.list[i]);
-        }
-      }
-      )
-      .catch(function(error){
-        console.error(error);
-      }
-      )
+          }
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
     })
     .catch(function (error) {
       console.error(error);
@@ -59,10 +57,16 @@ function getWeatherApi(event) {
 
 function printWeatherApi(resultsObj) {
   console.log(resultsObj);
-  var resultCard = document.createElement("div");
-  resultCard.classList.add("card", "bg-light", "text-dark", "mb-3", "p-3");
-  submitCityEl.append(resultCard);
+  var resultCard = document.createElement('div');
+  resultCard.classList.add('card', 'bg-light','text-dark')
+  if(resultsObj.main){
+    resultCard.innerHTML +=
+    '<strong>Main:,/strong>' + resultsObj.main.join(', ') + '<br/>';
+  } else{
+    resultCard.innerHTML +=
+    '<strong>Main:</strong> No main for this entry. ';
+  }
+  resultWeatherEl.append(resultCard);
 }
 
 document.getElementById("searchCity").addEventListener("click", getWeatherApi);
-
